95c6c2ce9aff16fe39c038cd31eed6b0c1f2deb2
Muzammil Hussain <muzammil.org73@gmail.com>
2026-06-09 21:56:21 +0500

    Add files via upload

---
+++ simple calculater.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a, b;
+    cout <<"enter two number of values"<<endl;
+    cin >>a,b;
+    int add= a+b;
+    cout <<"result is"<<add<<endl;
+    int subtract= a-b;
+    cout <<"result is"<<subtract<<endl;
+    int multiple= a*b;
+    cout <<"result is"<<multiple<<endl;
+    float divide= float(a/b);
+    cout <<"result is"<<divide<<endl;
+    return 0;
+}
